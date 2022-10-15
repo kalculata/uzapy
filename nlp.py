aa382dc787dcff507345a8dfef907466245c1873
kalculata <nprincehuzaifa@gmail.com>
2022-10-15 07:04:36 +0200

    one hot encoding

--- nlp.py
+++ nlp.py
@@ -1,2 +1,12 @@
-def one_hot_encoding():
-  pass
+import numpy as np
+
+
+def one_hot_encoding(classes, data):
+  y = []
+
+  for i in range(data.shape[0]):
+    one_hot = np.zeros(shape=(classes, 1))
+    one_hot[data[i]][0] = 1
+    y.append(one_hot)
+  
+  return np.array(y)
